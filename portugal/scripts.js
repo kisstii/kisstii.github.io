b4bf2ddd9343fc404a13c16ada799e16501b3e4a
kisstii <64484850+kisstii@users.noreply.github.com>
2020-05-07 19:45:56 +0200

    change

--- portugal/scripts.js
+++ portugal/scripts.js
@@ -1,6 +1,6 @@
 console.log('Script file is loaded.')
 
-let myImage = ['/images/1.jpg', '/images/2.jpg', '/images/3.jpg', '/images/4.jpg', '/images/5.jpg', '/images/6.jpg', '/images/7.jpg', '/images/8.jpg', '/images/9.jpg'];
+let myImage = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
 
 let title = ['Porto', 'Porto', 'Batalha', 'Tomar', 'Sintra', 'Sintra', 'Fatima', 'Ocean', 'Ocean']
 
